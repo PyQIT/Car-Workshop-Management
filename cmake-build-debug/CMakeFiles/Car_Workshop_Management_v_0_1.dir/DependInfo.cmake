f71ae86f1aa9aa818e363f9377f719c708789a7e
PyQIT <przempy@gmail.com>
2018-05-17 14:18:41 +0200

    Car-Workshop-Management

---
+++ cmake-build-debug/CMakeFiles/Car_Workshop_Management_v_0_1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Studia/Car-Workshop-Management_v_0.1/main.c" "C:/Studia/Car-Workshop-Management_v_0.1/cmake-build-debug/CMakeFiles/Car_Workshop_Management_v_0_1.dir/main.c.obj"
+  "C:/Studia/Car-Workshop-Management_v_0.1/sqlite3.c" "C:/Studia/Car-Workshop-Management_v_0.1/cmake-build-debug/CMakeFiles/Car_Workshop_Management_v_0_1.dir/sqlite3.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
